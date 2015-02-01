e9dde9c450eb0f92da257d9283fbcd0110ea9f02
STELIX <ssspinni@gmail.com>
2015-02-01 12:52:21 +0100

    Flo - STELIXize the device
    Change-Id: I08b23f49a40dc2de70700ae7d109ad3ac3ff82bb
    Signed-off-by: STELIX <ssspinni@gmail.com>

---
+++ carbon.mk
@@ -0,0 +1,27 @@
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1200
+
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/stelix/config/common_phone.mk)
+
+# Enhanced NFC
+#$(call inherit-product, vendor/stelix/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/flo/full_flo.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := flo
+PRODUCT_NAME := carbon_flo
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+CARBON_BUILDTYPE := NIGHTLY
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=razor \
+    BUILD_FINGERPRINT=google/razor/flo:5.0.2/LRX22G/1649326:user/release-keys \
+    PRIVATE_BUILD_DESC="razor-user 5.0.2 LRX22G 1649326 release-keys"
+
